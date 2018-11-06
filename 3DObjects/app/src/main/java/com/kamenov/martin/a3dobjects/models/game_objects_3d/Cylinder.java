fbc4a97b4b443e3f052e3e54597bec73f03748f9
MartinKamenov <martinkamenov10@gmail.com>
2018-11-06 17:23:39 +0200

    Added basic oval parts of cylinder

--- 3DObjects/app/src/main/java/com/kamenov/martin/a3dobjects/models/game_objects_3d/Cylinder.java
+++ 3DObjects/app/src/main/java/com/kamenov/martin/a3dobjects/models/game_objects_3d/Cylinder.java
@@ -2,7 +2,10 @@ package com.kamenov.martin.a3dobjects.models.game_objects_3d;
 
 import android.graphics.Paint;
 
+import com.kamenov.martin.a3dobjects.models.DeepPoint;
 import com.kamenov.martin.a3dobjects.models.game_objects_3d.contracts.Object3D;
+
+import java.util.ArrayList;
 
 /**
  * Created by Martin on 4.11.2018 г..
@@ -17,5 +20,25 @@ public class Cylinder extends Object3D {
         super(x, y, z, edgePaint, wallPaint, rotation);
         this.height = height;
         this.radius = radius;
+        float halfHeight = height/2;
+        DeepPoint a = new DeepPoint(0 - radius, 0, 0 - halfHeight);
+        DeepPoint b = new DeepPoint(0, 0 - radius, 0 - halfHeight);
+        DeepPoint c = new DeepPoint(0 + radius, 0, 0 - halfHeight);
+        DeepPoint d = new DeepPoint(0, 0 + radius, 0 - halfHeight);
+        DeepPoint a1 = new DeepPoint(0 - radius, 0, 0 + halfHeight);
+        DeepPoint b1 = new DeepPoint(0, 0 - radius, 0 + halfHeight);
+        DeepPoint c1 = new DeepPoint(0 + radius, 0, 0 + halfHeight);
+        DeepPoint d1 = new DeepPoint(0, 0 + radius, 0 + halfHeight);
+        points = new DeepPoint[] {
+            a, b ,c, d, a1, b1, c1, d1
+        };
+        parts = new ArrayList<>();
+        // Defines the two ovals of the cylinder
+        DeepPoint[] base = new DeepPoint[]{a, b, c, d};
+        DeepPoint[] top = new DeepPoint[]{a1, b1, c1, d1};
+        parts.add(base);
+        parts.add(top);
+
+        setDrawingParts();
     }
 }
