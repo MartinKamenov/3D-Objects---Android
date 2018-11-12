255601d19e3eb459bf01e5e9cd71d33a9def8d8b
MartinKamenov <martinkamenov10@gmail.com>
2018-11-12 22:36:04 +0200

    Added buttons infront the game pannel

--- 3DObjects/app/src/main/java/com/kamenov/martin/a3dobjects/game/GameActivity.java
+++ 3DObjects/app/src/main/java/com/kamenov/martin/a3dobjects/game/GameActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.kamenov.martin.a3dobjects.R;
 import com.kamenov.martin.a3dobjects.models.Constants;
 import com.kamenov.martin.a3dobjects.models.services.DrawingService;
 import com.kamenov.martin.a3dobjects.models.services.SortingService;
@@ -17,7 +20,12 @@ public class GameActivity extends Activity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(new GamePanel(this,
-                DrawingService.getInstance(SortingService.getInstance())));
+        setContentView(R.layout.activity_game);
+        RelativeLayout relativeLayout = findViewById(R.id.container);
+        GamePanel gamePanel = new GamePanel(this,
+                DrawingService.getInstance(SortingService.getInstance()));
+        relativeLayout.addView(gamePanel);
+        LinearLayout buttonContainer = findViewById(R.id.button_container);
+        buttonContainer.bringToFront();
     }
 }
